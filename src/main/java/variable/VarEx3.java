bbdd80e321aa3205f50a51fae96e7040cc6b418e
SE YEONG JEONG <claeo001@naver.com>
2023-08-17 23:38:00 +0900

    Variable start

--- src/main/java/variable/VarEx3.java
+++ src/main/java/variable/VarEx3.java
@@ -1,4 +1,23 @@
 package variable;
+/*
+     문자 하나를 감싸면 문자리터럴이라 부르고 두 문자 이상은 문자열 리터럴이다.
+     문자 리터럴은 공백이 있으면 안된다.
+     문자랑 타입변수를 더하면 문자열이 된다.
+ */
 
 public class VarEx3 {
+    public static void main(String[] args) {
+        String name = "Ja" + "va";
+        String str = name + 8.0;
+
+        System.out.println(name);
+        System.out.println(str);
+        System.out.println(7 + " ");
+        System.out.println(" "+ 7);
+        System.out.println(7 + "");
+        System.out.println("" + 7);
+        System.out.println("" + "");
+        System.out.println(7 + 7 + "");
+        System.out.println(""+ 7 + 7 );
+    }
 }
